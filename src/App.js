deb9987190ba5db227b5980a26b3ee046685cde3
ABDUHAMID <asasas>
2022-02-23 11:58:05 +0500

    ADDED NEW FEATURES

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Editor from "./components/Editor";
+import ExampleDocument from "./utils/ExampleDocument";
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [document, updateDocument] = useState(ExampleDocument);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Editor document={document} onChange={updateDocument} />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Editor.js
@@ -0,0 +1,92 @@
+import "./Editor.css";
+
+import { Editable, Slate, withReact, useSelected } from "slate-react";
+import {
+  identifyLinksInTextIfAny,
+  isLinkNodeAtSelection,
+} from "../utils/EditorUtils";
+import { useCallback, useMemo, useRef } from "react";
+
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import LinkEditor from "./LinkEditor";
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Toolbar from "./Toolbar";
+import { createEditor } from "slate";
+import useEditorConfig from "../hooks/useEditorConfig";
+import useSelection from "../hooks/useSelection";
+import { withHistory } from "slate-history";
+
+export default function Editor({ document, onChange }) {
+  const editorRef = useRef(null);
+  const editor = useMemo(() => withReact(withHistory(createEditor())), []);
+  const { renderLeaf, renderElement, KeyBindings } = useEditorConfig(editor);
+
+  const onKeyDown = useCallback(
+    (event) => KeyBindings.onKeyDown(editor, event),
+    [KeyBindings, editor]
+  );
+
+  const [previousSelection, selection, setSelection] = useSelection(editor);
+
+  const onChangeLocal = useCallback(
+    (doc) => {
+      onChange(doc);
+      setSelection(editor.selection);
+      identifyLinksInTextIfAny(editor);
+    },
+    [onChange, setSelection, editor]
+  );
+
+  let selectionForLink = null;
+  if (isLinkNodeAtSelection(editor, selection)) {
+    selectionForLink = selection;
+  } else if (
+    selection == null &&
+    isLinkNodeAtSelection(editor, previousSelection)
+  ) {
+    selectionForLink = previousSelection;
+  }
+
+  return (
+    <Slate editor={editor} value={document} onChange={onChangeLocal}>
+
+
+      <Container className={"editor-container"}>
+        <Row>
+          <Col>
+            <Toolbar
+              selection={selection}
+              previousSelection={previousSelection}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="editor" ref={editorRef}>
+              {selectionForLink != null ? (
+                <LinkEditor
+                  editorOffsets={
+                    editorRef.current != null
+                      ? {
+                          x: editorRef.current.getBoundingClientRect().x,
+                          y: editorRef.current.getBoundingClientRect().y,
+                        }
+                      : null
+                  }
+                  selectionForLink={selectionForLink}
+                />
+              ) : null}
+              <Editable
+                renderElement={renderElement}
+                renderLeaf={renderLeaf}
+                onKeyDown={onKeyDown}
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </Slate>
+  );
+}
